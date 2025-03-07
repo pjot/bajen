f07490319588a4bb0f7aec090bc5faacbad2e8f3
Peter Bergström <peter.bergstrom@eniro.com>
2025-03-07 08:26:32 +0100

    .dict, not .model_dump

--- tickets.py
+++ tickets.py
@@ -39,7 +39,7 @@ def read() -> dict[str, Game]:
 
 def save(games: list[Game]):
     with open("data.json", "w") as f:
-        json_data = {g.slug: g.model_dump() for g in games},
+        json_data = {g.slug: g.dict() for g in games},
         json.dump(json_data,f, indent=4)
 
 
